ab4348a9608d968be190b65cca05c998c09b1214
Deda228 <bigbergame@mail.ru>
2023-05-14 21:18:20 +0300

    [WIP]
    [WIP]
    
    
    [WIP] 3
    
    
    [WIP]
    
    
    [WIP]

--- Dinamic Diary.pas
+++ Dinamic Diary.pas
@@ -1,6 +1,10 @@
-﻿uses System.Globalization, 
-     System.IO,
-     System;
+﻿{$apptype windows}
+{$reference PresentationFramework.dll}
+{$reference PresentationCore.dll}
+{$reference WindowsBase.dll}
+
+uses System.Windows;
+uses System.Windows.Controls;
 
 {$region Type}
 
@@ -39,230 +43,46 @@ type
   
 {$endregion Type}
 
-{$region Compare}
-
-function CompareEntriesByTime(Entry1, Entry2: Entry): integer;
-begin
-  Result :=DateTime.Compare(Entry1.DateAndTime,Entry2.DateAndTime);
-end;
-
-{$endregion Compare}
-
-{$region Add}
-
-procedure AddEntry(Diary: List<Entry>);
-begin
-  try
-    Diary.Add(Entry.Create(DateTime.ParseExact(readstring('Введите дату (дд.мм.гггг чч:мм): '), 'dd.MM.yyyy HH:mm', new CultureInfo('ru-RU')),readstring('Введите описание к событию: '),false));
-  except
-    on e: System.FormatException do 
-      begin
-        println;
-        ('Ошибка при вводе даты и времени: ', e.Message).Println;
-        println;
-        exit;
-      end;
-  end;
-  println;
-  Diary.Sort(CompareEntriesByTime);
-end;
-
-{$endregion Add}
-
-{$region Print}
-
-procedure PrintEntries(Diary: List<Entry>);
-begin
-  var ru_ci := CultureInfo.CreateSpecificCulture('ru-RU');
-  if Diary.Count = 0 then
-  begin
-    'Дневник пуст.'.Println;
-    println;
-    Exit;
-  end;
-
-  'События в дневнике:'.Println;
-  println;
-
-  var CurrentDate := Diary[0].DateAndTime;
-  var CurrentYear := CurrentDate.Year;
-  var CurrentMonth := CurrentDate.Month;
-  var CurrentDayOfWeek := CurrentDate.DayOfWeek;
-
-  string.Format(ru_ci,'Год: {0}',CurrentYear).Println;
-  string.Format(ru_ci,'Месяц: {0}',Diary[0].DateAndTime.ToString('MMMM', CultureInfo.CurrentCulture)).Println;
-  string.Format(ru_ci,'День недели: {0}',Diary[0].DateAndTime.ToString('dddd', CultureInfo.CurrentCulture)).Println;
-
-  for var i := 0 to Diary.Count - 1 do
-  begin
-    if (Diary[i].DateAndTime.Year <> CurrentYear) then
-    begin
-      CurrentYear := Diary[i].DateAndTime.Year;
-      string.Format(ru_ci,'Год: {0}',CurrentYear).Println;
-    end;
-
-    if (Diary[i].DateAndTime.Month <> CurrentMonth) then
-    begin
-      CurrentMonth := Diary[i].DateAndTime.Month;
-      string.Format(ru_ci,'Месяц: {0}',Diary[i].DateAndTime.ToString('MMMM', CultureInfo.CurrentCulture)).Println;
-    end;
-
-    if Diary[i].DateAndTime.DayOfWeek <> CurrentDayOfWeek then
-    begin
-      CurrentDayOfWeek := Diary[i].DateAndTime.DayOfWeek;
-      string.Format(ru_ci,'День недели: {0}',Diary[i].DateAndTime.ToString('dddd', CultureInfo.CurrentCulture)).Println;
-    end;
-    println;
-    string.Format(ru_ci,'{0}. {1} - {2} - {3}',i+1 ,Diary[i].DateAndTime.ToString('dd.MM.yyyy HH:mm'), Diary[i].Task, Diary[i].Done).Println;
-    println;
-  end;
-
-  Println('-'*50);
-end;
-
-{$endregion Print}
-
-{$region Edit}
-
-procedure EditEntry(Diary: List<Entry>);
-begin
-  var Entry_Ind : integer;
-  repeat
-    Console.Clear; 
-    PrintEntries(Diary);
-    if Diary.Count = 0 then
-    begin
-      'Ежедневник пуст.'.Println;
-      exit;
-    end;
-    Entry_Ind := ReadLnInteger('Выберите порядковый номер записи, которую хотите изменить/удалить:') - 1;
-  until (cardinal(Entry_Ind) < Diary.Count);
- 
-  while true do
-  begin
-    Console.Clear;
-    'Что хотите изменить?'.PrintLn;
-    PrintLn;
-    'A - Дату и время события'.PrintLn;
-    'B - Описание события'.PrintLn;
-    'C - Статус события'.PrintLn;
-    'D - Удалить запись'.Println;
-    'X - Назад'.PrintLn;
-    println;
-    
-    var Choice := ReadLnChar('Выбор: ');
-    case upcase(choice) of
-      'A': 
-      begin
-        try
-          Diary[Entry_Ind] := Diary[Entry_Ind].WithDateAndTime(DateTime.ParseExact(readstring('Введите дату и время события (дд.мм.гггг чч:мм): '),'dd.MM.yyyy HH:mm', new CultureInfo('ru-RU')));
-        except
-          on e: System.FormatException do 
-          begin
-            ('Ошибка при вводе даты и времени: ', e.Message).Println;
-            break;
-          end;
-        end;
-      end;
-             
-      'B': Diary[Entry_Ind] := Diary[Entry_Ind].WithTask(ReadLnString('Введите новое описание события: '));
-
-      'C': Diary[Entry_Ind] := Diary[Entry_Ind].WithDone(ReadLnBoolean('Введите новый статус события (True/False): '));
-
-      'D': 
-      begin
-        Diary.RemoveAt(Entry_Ind);
-        'Запись удалена.'.Println;
-      end;
-        
-      'X': break;
-        
-      else 'Неправильный ввод данных.'.Println;
-        
-    end;
-  end; 
-  println;
-end;
-
-{$endregion Edit}
-
-{$region ToFile}
-
-procedure WriteToFile(Diary: List<Entry>);
-begin
-  var bw := new BinaryWriter(new FileStream('diary.bin', FileMode.Create));
-  for var i := 0 to Diary.Count - 1 do
-    begin
-      bw.Write(Diary[i].DateAndTime.Ticks);
-      bw.Write(Diary[i].Task);
-      bw.Write(Diary[i].Done);
-    end;
-  bw.Close();
-end;
-
-{$endregion toFile}
-
-{$region FromFile}
-
-function ReadFromFile(): List<Entry>;
-begin
-  var br := new BinaryReader(new FileStream('diary.bin', FileMode.OpenOrCreate));
-  var Diary := new List<Entry>;
-  while br.BaseStream.Position < br.BaseStream.Length do
-    begin
-      var dateTicks := br.ReadInt64();
-      var task := br.ReadString();
-      var done := br.ReadBoolean();
-      var new_entry: Entry := New Entry(new DateTime(dateTicks), task, done);
-      Diary.Add(new_entry);
-    end;
-  br.Close();
-  Diary.Sort(CompareEntriesByTime);
-  Result := Diary;
-end;
-
-{$endregion FromFile}
-
 {$region Menu}
 
 Procedure MainMenu(Diary: List<Entry>);
 begin
-  Diary := ReadFromFile;
-  while true do
-    begin
-      'A - Добавление события'.PrintLn;
-      'B - Просмотр событий'.PrintLn;
-      'C - Редактирование событий'.Println;
-      'D - Очистка ежедневника'.Println;
-      'X - Выход'.Println;
-      println;
-      var choice := ReadLnChar('Выбор: ');
-      Console.Clear;
-      case choice of
-        'A': AddEntry(Diary);
-        'B': PrintEntries(Diary);
-        'C': EditEntry(Diary);
-        'D': Diary.Clear;        
-        'X': 
-        begin
-          'До свидания!'.PrintLn;
-          break
-        end;
-        
-        else
-          begin
-            'Ваш выбор не соответствует предложенному, попробуйте еще раз'.Println;
-            println;
-          end;
-      end;
-    end;
-    WriteToFile(Diary);
+  var win := new window;
+  win.Name:='Diary';
+  var Okn := new DockPanel;
+  var SV := new ScrollViewer;
+  var CC := new ContentControl;
+  var Butcont := new StackPanel; 
+  Dockpanel.SetDock(Okn,Dock.Left);
+  SV.VerticalScrollBarVisibility := scrollbarvisibility.Auto;
+  SV.HorizontalScrollBarVisibility := scrollbarvisibility.Auto;
+  
+  var add_button := procedure(name: string; when_clicked: ()->object)->
+  begin
+    var button := new Button;
+    button.Content := name;
+    button.Width := 200;
+    button.Height := 100;
+    button.Click += (o,e) -> when_clicked();
+    Butcont.Children.Add(button);
+  end;
+  
+  //add_button('Добавить событие', ()->  );
+  //add_button('Показать  событие', ()->  );
+  //add_button('Редактировать событие', ()->  );
+  //add_button('Очистить ежедневник', ()->  );
+  //add_button('Выход', () -> );
+  
+  SV.Content := Butcont;
+  Okn.Children.Add(SV);
+  Okn.Children.Add(CC);
+  
+  win.Content := Okn;
+  Application.Create.Run(win);
 end;
 
 {$endregion Menu}
 
 begin
-  Console.Title := 'Ежедневник';
-  Console.ForegroundColor := ConsoleColor.Yellow;
   MainMenu(new List<Entry>);
 end.
